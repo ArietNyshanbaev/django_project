013495a8af524f941203c6866ac91f7768a482cb
Ariet <YOUR@EMAIL.com>
2014-09-27 16:12:35 +0600

    have changed some wievs

--- bestplayer/views.py
+++ bestplayer/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
 from bestplayer.models import Club
+from django.core.urlresolvers import reverse
 def index(request):
     top_five = Club.objects.order_by('name')[:5]
     return render(request,'bestplayer/index.html',{"top_five":top_five})
     
 def detail(request,club_id):
-    return HttpResponse("her is detail and clubs num is :",club_id )
+    club = get_object_or_404(Club,pk = club_id)
+    return render(request,'bestplayer/detail.html',{'club':club})
 def result(request,club_id):
-    return HttpResponse('her is result that is barraly working ')
+    return HttpResponse('congrats u have just voted')
 def vote (request,club_id):
-    return HttpResponse('vote is not in action now')
+    club = get_object_or_404(Club,pk = club_id)
+    return HttpResponseRedirect(reverse('bestplayer:result', args=(club.id,)))
