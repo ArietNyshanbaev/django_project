3376c5ff18c13dba378bf8c424a3d18f58650682
Ariet <YOUR@EMAIL.com>
2014-09-28 17:37:44 +0600

    have changed to genericViews

--- bestplayer/urls.py
+++ bestplayer/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url,patterns
 from bestplayer import views
 urlpatterns = patterns("",
-             url(r'^$', views.index, name='index'),
-             url(r'^(?P<club_id>\d+)/$',views.detail,name = 'detail'),
-             url(r'^(?P<club_id>\d+)/result/$',views.result,name = 'result'),
+             url(r'^$', views.IndexView.as_view(), name='index'),
+             url(r'^(?P<pk>\d+)/$',views.DetailView.as_view(),name = 'detail'),
+             url(r'^(?P<pk>\d+)/result/$',views.ResultView.as_view(),name = 'result'),
              url(r'^(?P<club_id>\d+)/vote/$',views.vote,name = 'vote'),
              )
--- bestplayer/views.py
+++ bestplayer/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from bestplayer.models import Club,Footballer
 from django.core.urlresolvers import reverse
-def index(request):
+from django.views import generic
+
+
+"""def index(request):
     top_five = Club.objects.order_by('name')[:5]
     return render(request,'bestplayer/index.html',{"top_five":top_five})
     
@@ -12,6 +15,24 @@ def detail(request,club_id):
 def result(request,club_id):
     club = get_object_or_404(Club,pk = club_id)
     return render(request,'bestplayer/result.html',{'club':club})
+"""
+
+class IndexView(generic.ListView):
+    template_name = 'bestplayer/index.html'
+    context_object_name = 'top_five'
+    
+    def get_queryset(self):
+        return Club.objects.order_by('name')[:5]
+
+class DetailView(generic.DetailView):
+    model = Club
+    template_name = 'bestplayer/detail.html'
+    
+class ResultView(generic.DetailView):
+    model = Club
+    template_name = 'bestplayer/result.html'
+
+
 def vote (request,club_id):
     club = get_object_or_404(Club,pk = club_id)
     try:
